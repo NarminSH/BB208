dccf6d3487435c865c9a69d5bf719e91c077c5a9
NarminSH <nshivakhanova@gmail.com>
2025-04-30 16:49:18 +0400

    MVC front to back first project

---
+++ BB208MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿
+using BB208MVCIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace BB208MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Service s1 = new Service()
+            {
+                Id = 1,
+                Title = "Backend development",
+                Description = "asdfghjklfjhgdsdfghjhdhsgfa",
+                ImgUrl = "assets/img/services-01.jpg"
+            };
+            Service s2 = new Service()
+            {
+                Id = 2,
+                Title = "Social media",
+                Description = "asdfghjklfjhgdsdfghjhdhsgfa",
+                ImgUrl = "assets/img/services-02.jpg"
+            };
+            Service s3 = new Service()
+            {
+                Id = 3,
+                Title = "Marketing",
+                Description = "asdfghjklfjhgdsdfghjhdhsgfa",
+                ImgUrl = "assets/img/services-03.jpg"
+            };
+            List<Service> services = new List<Service>() { s1, s2, s3 };
+            return View(services);
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ BB208MVCIntro/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace BB208MVCIntro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
